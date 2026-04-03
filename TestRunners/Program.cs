1f144f526102f0b8790ed622faa0c4237e0d0caf
Papaj1234 <germann2007simm@gmail.com>
2026-04-03 13:16:18 +0300

    Add TestRunnres

---
+++ TestRunners/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestRunners
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
